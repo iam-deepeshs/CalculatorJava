b537a77c423a2a848dc6a21a54d4aa13bf2b371f
Deepesh Srivastava <121711203+iam-deepeshs@users.noreply.github.com>
2025-04-18 23:19:37 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,30 @@
+public class Operations {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public double square(double a) {
+        return a * a;
+    }
+
+    public double cube(double a) {
+        return a * a * a;
+    }
+
+    public double squareRoot(double a) {
+        return Math.sqrt(a);
+    }
+}
